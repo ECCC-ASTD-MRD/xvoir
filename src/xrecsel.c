7014800eeec31b052b5cbaa132ce32c3c2623669
Chantal Pic <chantal.pic@ec.gc.ca>
2025-03-05 12:31:13 -0500

    Fix warning with gnu: unnecessary usage of calloc

--- src/xrecsel.c
+++ src/xrecsel.c
@@ -2773,8 +2773,8 @@ void XSelectstdCommencerInit(char *titre, int32_t nbrecs, char **idents, int32_t
       xs[wi].indRecsFiltres        = (int32_t *)calloc(xs[wi].nbRecs, sizeof(int));
       xs[wi].indRecsSelect         = (int32_t *)calloc(xs[wi].nbRecs, sizeof(int));
       xs[wi].recsAffiches          = (XmString *)calloc(nbrecs, sizeof(XmString));
-      xs[wi].recs                  = (XmString **)calloc(nbdes, sizeof(XmString *)); 
-      
+      xs[wi].recs                  = (XmString **)malloc(nbdes * sizeof(XmString *)); // ensuite initialisé dans la boucle ci-dessous
+
       for (i = 0; i < nbdes; i++) 
          xs[wi].recs[i] = (XmString *)calloc(nbrecs, sizeof(XmString));
    }
